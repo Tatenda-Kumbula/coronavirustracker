3a8add76cd986293c3a32ad26d864c97d4e48577
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-03-26 13:20:48 +0200

    Add files via upload

---
+++ Coronavirus-trackerfull/src/main/java/Corona/Coronavirustrackerfull/Service.java
@@ -0,0 +1,5 @@
+package Corona.Coronavirustrackerfull;
+
+public @interface Service {
+
+}
---
+++ Coronavirus-trackerfull/src/test/java/Corona/Coronavirustrackerfull/CoronavirusTrackerfullApplicationTests.java
@@ -0,0 +1,13 @@
+package Corona.Coronavirustrackerfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
